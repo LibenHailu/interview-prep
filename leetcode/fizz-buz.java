80e2794c252014d37fdb6ad4d3d5fd4f02dad610
LibenHailu <libenhailu04@gmail.com>
2021-12-15 17:53:19 +0000

    first commit

---
+++ leetcode/fizz-buz.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+
+        for (int i = 1; i <= n; i++) {
+            // checking if the number is divisible by 3 and 5 with out a remender
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            }
+            // checking if the number is divisible by 3 with out a remender
+            else if (i % 3 == 0) {
+                answer.add("Fizz");
+            }
+            // checking if the number is divisible by 5 with out a remender
+            else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(Integer.toString(i));
+            }
+        }
+
+        return answer;
+    }
+}
