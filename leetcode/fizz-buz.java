c5efd12fcaedcff7f0f9f947c9893493c9c3ea85
LibenHailu <libenhailu04@gmail.com>
2021-12-16 10:31:08 +0300

    students grade

--- leetcode/fizz-buz.java
+++ leetcode/fizz-buz.java
@@ -1,31 +1,31 @@
-package leetcode;
+// package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-// https://leetcode.com/problems/fizz-buzz/submissions/
+// // https://leetcode.com/problems/fizz-buzz/submissions/
 
-class Solution {
-    public List<String> fizzBuzz(int n) {
-        List<String> answer = new ArrayList<String>();
+// class Solution {
+//     public List<String> fizzBuzz(int n) {
+//         List<String> answer = new ArrayList<String>();
 
-        for (int i = 1; i <= n; i++) {
-            // checking if the number is divisible by 3 and 5 with out a remender
-            if (i % 3 == 0 && i % 5 == 0) {
-                answer.add("FizzBuzz");
-            }
-            // checking if the number is divisible by 3 with out a remender
-            else if (i % 3 == 0) {
-                answer.add("Fizz");
-            }
-            // checking if the number is divisible by 5 with out a remender
-            else if (i % 5 == 0) {
-                answer.add("Buzz");
-            } else {
-                answer.add(Integer.toString(i));
-            }
-        }
+//         for (int i = 1; i <= n; i++) {
+//             // checking if the number is divisible by 3 and 5 with out a remender
+//             if (i % 3 == 0 && i % 5 == 0) {
+//                 answer.add("FizzBuzz");
+//             }
+//             // checking if the number is divisible by 3 with out a remender
+//             else if (i % 3 == 0) {
+//                 answer.add("Fizz");
+//             }
+//             // checking if the number is divisible by 5 with out a remender
+//             else if (i % 5 == 0) {
+//                 answer.add("Buzz");
+//             } else {
+//                 answer.add(Integer.toString(i));
+//             }
+//         }
 
-        return answer;
-    }
-}
+//         return answer;
+//     }
+// }
