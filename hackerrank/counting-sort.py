7713e12a55b473477c431c167f6558decbf9dfe2
LibenHailu <libenhailu04@gmail.com>
2021-12-17 20:50:24 +0300

    counting sort part 1

---
+++ hackerrank/counting-sort.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+""""
+https://www.hackerrank.com/challenges/countingsort1/problem
+"""
+
+def countingSort(arr):
+    # Write your code here 
+    count = [0] * 100
+    
+    for num in arr:
+        count[num] += 1
+        
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
