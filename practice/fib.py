eb232e90e20952d5729af274665accd61889e125
LibenHailu <libenhailu04@gmail.com>
2022-03-10 16:12:34 +0300

    last-boot

--- practice/fib.py
+++ practice/fib.py
@@ -1,58 +1,74 @@
-# the recursive approach
+# # the recursive approach
 
-# def f(i):
-#     if i < 2:
-#         return i
-# if is_computed[i]:
-#     return f[i]
+# # def f(i):
+# #     if i < 2:
+# #         return i
+# # if is_computed[i]:
+# #     return f[i]
 
-# f[i] = f(i-1) + f(i - 2)
-# is_computed[i] = True
-# return f[i]
-# return f(i-1) + f(i - 2)
-
-
-# Function for nth Fibonacci number
-def Fibonacci(n):
-
-    # Check if input is 0 then it will
-    # print incorrect input
-    if n < 0:
-        print("Incorrect input")
-
-    # Check if n is 0
-    # then it will return 0
-    elif n == 0:
-        return 0
-
-    # Check if n is 1,2
-    # it will return 1
-    elif n == 1 or n == 2:
-        return 1
-
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+# # f[i] = f(i-1) + f(i - 2)
+# # is_computed[i] = True
+# # return f[i]
+# # return f(i-1) + f(i - 2)
 
 
-print(Fibonacci(9))
+# # Function for nth Fibonacci number
+# def Fibonacci(n):
 
-# with dp
+#     # Check if input is 0 then it will
+#     # print incorrect input
+#     if n < 0:
+#         print("Incorrect input")
+
+#     # Check if n is 0
+#     # then it will return 0
+#     elif n == 0:
+#         return 0
+
+#     # Check if n is 1,2
+#     # it will return 1
+#     elif n == 1 or n == 2:
+#         return 1
+
+#     else:
+#         return Fibonacci(n-1) + Fibonacci(n-2)
 
 
-def fib(n):
+# print(Fibonacci(9))
 
-    f = [0] * n
-    f[0] = 1
-    f[1] = 1
-
-    for i in range(2, n):
-        f[i] = f[i-1] + f[i - 2]
-
-    return f[n - 1]
+# # with dp
 
 
-print(fib(9))
+# def fib(n):
+
+#     f = [0] * n
+#     f[0] = 1
+#     f[1] = 1
+
+#     for i in range(2, n):
+#         f[i] = f[i-1] + f[i - 2]
+
+#     return f[n - 1]
+
+
+# print(fib(9))
 # if n == 0:
 #     return 0
 # if n == 1:
 #     return 1
+
+f = [0] * 50
+
+
+def Fibonacci(n):
+    if f[n] == 0:
+        if n == 0:
+            f[n] = 0
+        elif n == 1:
+            f[n] = 1
+        else:
+            f[n] = Fibonacci(n-1) + Fibonacci(n-2)
+    return f[n]
+
+
+print(Fibonacci(9))
